df20fc584ae339f77f9b4809505fb835e0c53d33
N K Vinay Gowda <vinaygowda92413@gmail.com>
2020-08-28 17:12:21 +0530

    for loop added

---
+++ NestedFor.java
@@ -0,0 +1,38 @@
+package tut;
+
+public class NestedFor {
+
+	public static void main(String[] args) {
+		
+		int n=10;
+		for(int i=1;i<=n;i++) {
+			
+			for(int j=1;j<=i;j++) {
+				if(j==i || i==n || j==1)
+					System.out.print("*  ");
+				
+				else
+					System.out.print("   ");
+					
+			}
+			System.out.println();
+		}
+			
+			}
+			
+			
+			}
+			
+			
+			
+				
+			
+			
+			
+			
+		
+		
+
+	
+
+
